60e8232ba9506732ac0aa3af09fe4df2fe653d4b
David Sloane <david@cambrean.com>
2024-03-25 21:50:37 +0000

    improved UI, simple starting page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 import { Separator } from "@/components/ui/separator"
+import { Novatrix } from "uvcanvas"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="space-y-1">
+    <div className="flex flex-col p-32 h-screen">
+      <div className="space-y-2">
         <h4 className="text-md font-medium leading-none">David Sloane</h4>
         <p className="text-sm text-muted-foreground">
-        Designer, Founder. 
+          Designer, Founder.
         </p>
+        {/* Wrap "about", "design", "work" in their own div and add margin-top */}
+        <div className="pt-8">
+          <Link href="/about" passHref>
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+              about
+            </p>
+          </Link>
+          <Link href="/about" passHref>
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+              design
+            </p>
+          </Link>
+          <Link href="/about" passHref>
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+              work
+            </p>
+          </Link>
+        </div>
       </div>
-      <Separator className="my-4 max-w-36" />
-      <div className="flex h-5 space-x-4 text-sm">
-      <div>Work</div>
-      <Separator orientation="vertical" />
-      <div>Notes</div>
     </div>
-  </div>
   );
 }
-
-
