6fdefb29e59122ebba12b06add5d50600805a9d6
David Sloane <david@cambrean.com>
2024-03-26 17:09:11 +0000

    bug fix 🐛🐛

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col p-32 h-screen">
+    <div className="flex p-32 h-screen">
       <div className="space-y-2">
         <h4 className="text-md font-medium leading-none">David Sloane</h4>
         <p className="text-sm text-muted-foreground">
@@ -14,17 +14,17 @@ export default function Home() {
         {/* Wrap "about", "design", "work" in their own div and add margin-top */}
         <div className="pt-8">
           <Link href="/about" passHref>
-            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25">
               about
             </p>
           </Link>
           <Link href="/about" passHref>
-            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25">
               design
             </p>
           </Link>
           <Link href="/about" passHref>
-            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25 -webkit-font-smoothing: antialiased;">
+            <p className="cursor-pointer pt-2 text-sm text-muted-foreground transition-opacity duration-150 hover:opacity-25">
               work
             </p>
           </Link>
