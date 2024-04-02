2ee6b6243b92bba5a27ec56b45316a490ce308e5
David Sloane <david@cambrean.com>
2024-04-02 22:48:20 +0100

    edits

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,7 @@
 export default function About() {
     return (
         <div className="space-y-2">
-            <h4 className="text-md font-medium leading-none">About</h4>
+            <h4 className="text-base font-sans font-medium">About</h4>
             <p className="text-sm text-muted-foreground">
           fill in</p>
         </div>
