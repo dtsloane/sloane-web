c30a6b4c292187b1bd068bf53cb3c21dd47f1c9a
David Sloane <david@cambrean.com>
2024-04-10 16:11:15 +0100

    added breadcrumbs

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,14 @@
 import 'tailwindcss/tailwind.css';
+import { Slash } from "lucide-react"
+ 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
 export default function About() {
     return (
@@ -8,6 +18,25 @@ export default function About() {
                 <p className="text-sm text-muted-foreground">
                 Born in Cork, Ireland in '02. 
                 </p>
+                <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <Slash />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <Slash />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
             </div>
         </div>
     );
