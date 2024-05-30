ee2ca50c217d2e622f82bb9dc5f7887150b7c4f8
David Sloane <david@cambrean.com>
2024-05-30 14:48:04 +0100

    added images but it's terrible looking - will fix later

--- src/components/ui/PortfolioGrid.tsx
+++ src/components/ui/PortfolioGrid.tsx
@@ -2,31 +2,26 @@ import Image from 'next/image';
 import React from 'react';
 
 const images = [
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg",
-  "/placeholder.svg"
+  "/portfolio/HRV baselines.png",
+  "/portfolio/influences-marketing.png",
+  "/portfolio/linkedin-polish.post.png",
+  "/portfolio/tags-simple.png",
+  "/portfolio/workouts-thumbnail.png",
 ];
 
 const PortfolioGrid: React.FC = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+    <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
       {images.map((src, index) => (
         <div key={index} className="relative group">
           <Image
             alt={`Image ${index + 1}`}
-            className="w-full h-full object-cover rounded-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-lg"
+            className="object-cover rounded-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-lg"
             src={src}
-            width={300}
-            height={300}
+            width={800} // Adjusted width
+            height={600} // Adjusted height to maintain aspect ratio
             layout="responsive"
-            objectFit="cover"
+            objectFit="contain" // Changed to contain to maintain aspect ratio without cropping
           />
         </div>
       ))}
--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import PortfolioGrid from '../components/ui/PortfolioGrid'; // Ensure the path is correct
+import PortfolioGrid from '../components/ui/PortfolioGrid';
+import '../app/globals.css';
 
 const Design: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-8">Portfolio</h1>
+      <h1 className="text-lg md:text-md font-medium space-y-8">Portfolio</h1>
       <PortfolioGrid />
     </div>
   );
