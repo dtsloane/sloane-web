3b7bc5bf6e179bff3bbe51cbd3741e93080ed8c2
David Sloane <david@cambrean.com>
2024-05-31 12:07:17 +0100

    more updates to design / porfolio - incl image open

--- src/components/ui/PortfolioGrid.tsx
+++ src/components/ui/PortfolioGrid.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 const images = [
   "/portfolio/HRV baselines.png",
@@ -10,23 +10,50 @@ const images = [
 ];
 
 const PortfolioGrid: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const openModal = (src: string) => {
+    setSelectedImage(src);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
-      {images.map((src, index) => (
-        <div key={index} className="relative group">
-          <Image
-            alt={`Image ${index + 1}`}
-            className="object-cover rounded-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-lg"
-            src={src}
-            width={800} // Adjusted width
-            height={600} // Adjusted height to maintain aspect ratio
-            layout="responsive"
-            objectFit="contain" // Changed to contain to maintain aspect ratio without cropping
-          />
+    <div className="relative">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 p-6">
+        {images.map((src, index) => (
+          <div key={index} className="relative group cursor-pointer" onClick={() => openModal(src)}>
+            <div className="relative w-full h-0 pb-[75%] overflow-hidden">
+              <Image
+                alt={`Image ${index + 1}`}
+                className="absolute top-0 left-0 w-full h-full object-contain rounded-md transition-transform duration-300 group-hover:scale-105 group-hover:shadow-lg"
+                src={src}
+                fill
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm" onClick={closeModal}>
+          <div className="relative w-3/4 h-3/4 bg-transparent rounded-lg overflow-hidden" onClick={(e) => e.stopPropagation()}>
+            <button className="absolute top-2 right-2 text-black" onClick={closeModal}>✖</button>
+            <div className="relative w-full h-full">
+              <Image
+                alt="Selected Image"
+                className="w-full h-full object-contain"
+                src={selectedImage}
+                fill
+              />
+            </div>
+          </div>
         </div>
-      ))}
+      )}
     </div>
   );
-}
+};
 
 export default PortfolioGrid;
--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -5,8 +5,10 @@ import '../app/globals.css';
 const Design: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-lg md:text-md font-medium space-y-8">Portfolio</h1>
-      <PortfolioGrid />
+      <h1 className="text-lg md:text-md font-medium mb-8">Portfolio</h1>
+      <div className="w-full px-6">
+        <PortfolioGrid />
+      </div>
     </div>
   );
 }
