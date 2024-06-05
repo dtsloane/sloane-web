64f7ba8b7154e6309aa605c90a3b5241a3c3edc7
David Sloane <david@cambrean.com>
2024-06-05 16:18:10 +0100

    design page

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -12,8 +12,12 @@ import {
 
 const Design: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-lg md:text-md font-medium mb-8">Portfolio</h1>
+    <div className="flex flex-row items-start justify-center p-8 md:p-32 min-h-screen">
+      <div className="flex flex-col space-y-8 text-left max-w-lg w-full">
+                 <header className="space-y-2">
+                    <h1 className="text-lg md:text-md font-medium">Design</h1>
+                    <h2 className="text-sm text-muted-foreground">Portfolio</h2>
+                </header>
           <Breadcrumb>
             <BreadcrumbList>
                     <BreadcrumbItem>
@@ -25,6 +29,7 @@ const Design: React.FC = () => {
                     </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
+          </div>
       <div className="w-full px-6">
         <PortfolioGrid />
       </div>
