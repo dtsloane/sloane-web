d932a75c9f4330efdf3a3045b931730d11c793cc
David Sloane <david@cambrean.com>
2024-06-05 18:06:43 +0100

    adjustments

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -12,24 +12,24 @@ import {
 
 const Design: React.FC = () => {
   return (
-    <div className="flex flex-row items-start justify-center p-8 md:p-32 min-h-screen">
-      <div className="flex flex-col space-y-8 text-left max-w-lg w-full">
+    <div className="flex flex-col items-start justify-center p-8 md:p-32 min-h-screen">
+      <div className="flex flex-col space-y-8 max-w-lg w-full">
+                <Breadcrumb>
+                      <BreadcrumbList>
+                              <BreadcrumbItem>
+                              <BreadcrumbLink href="/">Back</BreadcrumbLink>
+                              </BreadcrumbItem>
+                              <BreadcrumbSeparator />
+                              <BreadcrumbItem>
+                              <BreadcrumbPage>Design</BreadcrumbPage>
+                              </BreadcrumbItem>
+                      </BreadcrumbList>
+                  </Breadcrumb>
                  <header className="space-y-2">
                     <h1 className="text-lg md:text-md font-medium">Design</h1>
                     <h2 className="text-sm text-muted-foreground">Portfolio</h2>
                 </header>
-          <Breadcrumb>
-            <BreadcrumbList>
-                    <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Back</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                    <BreadcrumbPage>Design</BreadcrumbPage>
-                    </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-          </div>
+        </div>
       <div className="w-full px-6">
         <PortfolioGrid />
       </div>
