16fcd50cd1ef16822cfd6aedcc8be7a0c57c8d28
David Sloane <david@cambrean.com>
2024-07-01 09:58:11 +0100

    started playing with framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-
+"use client";
 import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/legacy/image';
 import './globals.css';
+import { motion } from 'framer-motion';
 
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
-    <div className="flex flex-row items-start justify-center p-8 md:p-32 h-screen">
-      <div className="flex flex-col items-start space-y-4 w-full md:w-auto text-left">
+    <motion.div 
+      className={`flex flex-row items-start justify-center p-8 md:p-32 h-screen ${isLoaded ? 'no-blur' : 'blur-effect'}`}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.div 
+        className={`flex flex-col items-start space-y-4 w-full md:w-auto text-left ${isLoaded ? 'no-blur' : 'blur-effect'}`}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 0.8 }}
+      >
         <h4 className="text-lg md:text-md font-medium leading-none">David Sloane</h4>
         <p className="text-sm text-muted-foreground">Designer, Founder.</p>
         <div className="pt-8 space-y-2">
@@ -31,15 +49,20 @@ export default function Home() {
             </p>
           </Link>
         </div>
-      </div>
-      <div className="ml-4 md:ml-64 mt-0 flex-shrink-0 w-[100px] h-[100px] border-2 border-slate-300 relative transition-transform duration-500 hover:scale-105">
+      </motion.div>
+      <motion.div 
+        className={`ml-4 md:ml-64 mt-0 flex-shrink-0 w-[100px] h-[100px] border-2 border-slate-300 relative transition-transform duration-500 hover:scale-105 ${isLoaded ? 'no-blur' : 'blur-effect'}`}
+        initial={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.25, duration: 0.25 }}
+      >
         <Image
           src="/profile-pics/david.profile  copy.jpeg"
           alt="Profile Picture"
           layout="fill"
           style={{ objectFit: 'cover', width: '100%', height: '100%' }}
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
