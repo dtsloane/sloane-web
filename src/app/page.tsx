a959f0388bc285c85095bc9b9f1d012d274d5ff6
David Sloane <david@cambrean.com>
2024-07-01 19:06:41 +0100

    tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,22 +29,22 @@ export default function Home() {
         <p className="text-sm text-muted-foreground">Designer, Founder.</p>
         <div className="pt-8 space-y-2">
           <Link href="/about" passHref>
-            <p className="cursor-pointer text-sm text-muted-foreground transition-opacity duration-150  hover:bg-gray-50 p-2 rounded-lg space-y-2">
+            <p className="cursor-pointer text-sm text-muted-foreground hover:text-gray-600 transition-opacity duration-150  hover:bg-gray-100 p-2 rounded-lg space-y-2">
               about
             </p>
           </Link>
           <Link href="/design" passHref>
-            <p className="cursor-pointer text-sm text-muted-foreground transition-opacity duration-150  hover:bg-gray-50 p-2 rounded-lg space-y-2">
+            <p className="cursor-pointer text-sm text-muted-foreground hover:text-gray-600 transition-opacity duration-150  hover:bg-gray-100 p-2 rounded-lg space-y-2">
               design
             </p>
           </Link>
           <Link href="/work" passHref>
-            <p className="cursor-pointer text-sm text-muted-foreground transition-opacity duration-150  hover:bg-gray-50 p-2 rounded-lg space-y-2">
+            <p className="cursor-pointer text-sm text-muted-foreground hover:text-gray-600 transition-opacity duration-150  hover:bg-gray-100 p-2 rounded-lg space-y-2">
               work
             </p>
           </Link>
           <Link href="/reading" passHref>
-            <p className="cursor-pointer text-sm text-muted-foreground transition-opacity duration-150 hover:bg-gray-50 p-2 rounded-lg space-y-2">
+            <p className="cursor-pointer text-sm text-muted-foreground hover:text-gray-600 transition-opacity duration-150 hover:bg-gray-100 p-2 rounded-lg space-y-2">
               reading
             </p>
           </Link>
