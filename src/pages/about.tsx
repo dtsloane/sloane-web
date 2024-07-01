8f497b724c0a2df257ae79a597440aea2c5c548a
David Sloane <david@cambrean.com>
2024-07-01 19:34:38 +0100

    merged work with about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,6 +13,22 @@ import {
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
+const recognitionLinks = [
+    {
+        href: 'https://www.independent.ie/business/irish/30-under-30-young-guns-who-are-shooting-for-the-stars/41045671.html',
+        text: "30 under 30 (independent)",
+    },
+    {
+        href: 'https://x.com/ProductHunt/status/1699101383629443306',
+        text: 'Product Hunt',
+    },
+    {
+        href: 'https://www.businesspost.ie/connected/irish-techs-young-guns-this-years-30-under-30/',
+        text: '30 under 30 (business post)',
+    },
+    // more links
+];
+
 const About: React.FC = () => {
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -44,11 +60,39 @@ const About: React.FC = () => {
                         </BreadcrumbItem>
                     </BreadcrumbList>
                 </Breadcrumb>
-                {/* Header Section */}
-                <header className="space-y-2">
-                    <h1 className="text-lg md:text-md font-medium">About</h1>
-                    <h2 className="text-sm text-muted-foreground">Designer, Builder, Founder.</h2>
-                </header>
+                <div className="mb-8">
+                    <h4 className="text-lg md:text-md font-medium">Work</h4>
+                    <div className="space-y-4">
+                        <a href="https://cambrean.com" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-4 mt-4 transition-colors duration-150 hover:bg-gray-100 p-2 rounded-lg">
+                            <img src="/Cambrean.Logo.png" alt="Cambrean Logo" className="w-7 h-7 rounded-lg transition-transform duration-300 hover:scale-110" />
+                            <div className='flex-col'>
+                                <span className="text-sm cursor-pointer">
+                                    Cambrean
+                                </span>
+                                <p className='text-sm text-muted-foreground'>
+                                    Generate better health insights
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className='space-y-2 pt-2'>
+                    <h4 className="text-md md:text-md font-medium">Recognition</h4>
+                    <div className='space-y-2'>
+                        {recognitionLinks.map((link, index) => (
+                            <a
+                                key={index}
+                                href={link.href}
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                className='text-sm text-slate-400 hover:text-slate-500 cursor-pointer transition-colors duration-150 hover:bg-gray-100 p-2 rounded-lg block'
+                            >
+                                {link.text}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+
 
                 {/* Timeline Section */}
                 <section className="space-y-6">
